307b61a4468be3b665f8d71cd052aef76f272929
tommy <tommyfenv@163.com>
2018-10-08 23:53:54 +0800

    添加 wifi 相关工具类

---
+++ library/src/main/java/com/cyy/utils/WifiUtils.java
@@ -0,0 +1,302 @@
+package com.cyy.utils;
+
+import android.content.Context;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.text.TextUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+
+/**
+ * @author Guojin
+ */
+@RequiresApi(api = Build.VERSION_CODES.KITKAT)
+public class WifiUtils {
+
+    public enum WifiCipherType {
+        WIFICIPHER_WEP, WIFICIPHER_WPA, WIFICIPHER_NOPASS, WIFICIPHER_INVALID
+    }
+
+    private WifiUtils() {
+    }
+
+
+    public static List<ScanResult> getWifiScanResult(Context context) {
+        return ((WifiManager) Objects.requireNonNull(context.getApplicationContext().getSystemService(Context.WIFI_SERVICE))).getScanResults();
+    }
+
+    public static boolean isWifiEnable(Context context) {
+        return ((WifiManager) Objects.requireNonNull(context.getApplicationContext().getSystemService(Context.WIFI_SERVICE))).isWifiEnabled();
+    }
+
+    public static WifiInfo getConnectedWifiInfo(Context context) {
+        return ((WifiManager) Objects.requireNonNull(context.getApplicationContext().getSystemService(Context.WIFI_SERVICE))).getConnectionInfo();
+    }
+
+    public static List getConfigurations(Context context) {
+        return ((WifiManager) Objects.requireNonNull(context.getApplicationContext().getSystemService(Context.WIFI_SERVICE))).getConfiguredNetworks();
+    }
+
+
+    /**
+     * 创建一个配置文件，用于WIFI 的连接
+     *
+     * @param SSID     wifi 名称
+     * @param password wifi 密码
+     * @param type     wifi 类型
+     * @return wifi 配置文件
+     */
+    public static WifiConfiguration createWifiConfig(String SSID, String password, WifiCipherType type) {
+
+        WifiConfiguration config = new WifiConfiguration();
+        config.allowedAuthAlgorithms.clear();
+        config.allowedGroupCiphers.clear();
+        config.allowedKeyManagement.clear();
+        config.allowedPairwiseCiphers.clear();
+        config.allowedProtocols.clear();
+        config.SSID = "\"" + SSID + "\"";
+
+        if (type == WifiCipherType.WIFICIPHER_NOPASS) {
+//            config.wepKeys[0] = "";  //注意这里
+            config.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+//            config.wepTxKeyIndex = 0;
+        }
+
+        if (type == WifiCipherType.WIFICIPHER_WEP) {
+            config.preSharedKey = "\"" + password + "\"";
+            config.hiddenSSID = true;
+            config.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP104);
+            config.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+            config.wepTxKeyIndex = 0;
+        }
+
+        if (type == WifiCipherType.WIFICIPHER_WPA) {
+            config.preSharedKey = "\"" + password + "\"";
+            config.hiddenSSID = true;
+            config.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+            config.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
+            config.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+            config.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+            config.status = WifiConfiguration.Status.ENABLED;
+
+        }
+
+        return config;
+
+    }
+
+
+    /**
+     * 接入一个指定的wifi热点
+     *
+     * @param config  wifi 配置文件，包含 wifi 的所有信息
+     * @param context
+     * @return
+     */
+    public static boolean addNetWork(WifiConfiguration config, Context context) {
+
+        WifiManager wifimanager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        if (wifimanager == null) return false;
+
+        WifiInfo wifiinfo = wifimanager.getConnectionInfo();
+
+        if (null != wifiinfo) {
+            wifimanager.disableNetwork(wifiinfo.getNetworkId());
+        }
+
+        boolean result;
+
+        if (config.networkId > 0) {
+            result = wifimanager.enableNetwork(config.networkId, true);
+            wifimanager.updateNetwork(config);
+        } else {
+
+            int i = wifimanager.addNetwork(config);
+            result = false;
+
+            if (i > 0) {
+                wifimanager.saveConfiguration();
+                return wifimanager.enableNetwork(i, true);
+            }
+        }
+
+        return result;
+
+    }
+
+    /**
+     * 判断wifi热点支持的加密方式
+     *
+     * @param s
+     * @return
+     */
+    public static WifiCipherType getWifiCipher(String s) {
+
+        if (s.isEmpty()) {
+            return WifiCipherType.WIFICIPHER_INVALID;
+        } else if (s.contains("WEP")) {
+            return WifiCipherType.WIFICIPHER_WEP;
+        } else if (s.contains("WPA") || s.contains("WPA2") || s.contains("WPS")) {
+            return WifiCipherType.WIFICIPHER_WPA;
+        } else {
+            return WifiCipherType.WIFICIPHER_NOPASS;
+        }
+    }
+
+    /**
+     * 查看以前是否也配置过该网络
+     *
+     * @param SSID    wifi 名称
+     * @param context
+     * @return 已配置的 Wifi 的配置信息
+     */
+    public static WifiConfiguration hasConfigured(String SSID, Context context) {
+        WifiManager wifimanager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        if (wifimanager == null) return null;
+        List<WifiConfiguration> existingConfigs = wifimanager.getConfiguredNetworks();
+        for (WifiConfiguration existingConfig : existingConfigs) {
+            if (existingConfig.SSID.equals("\"" + SSID + "\"")) {
+                return existingConfig;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 设置：打开 wifi 功能
+     *
+     * @param context
+     */
+    public static void openWifi(Context context) {
+        WifiManager wifimanager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        if (wifimanager == null) return;
+        if (!wifimanager.isWifiEnabled()) {
+            wifimanager.setWifiEnabled(true);
+        }
+    }
+
+    /**
+     * 设置：关闭 wifi 功能
+     *
+     * @param context
+     */
+    public static void closeWifi(Context context) {
+        WifiManager wifimanager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        if (wifimanager == null) return;
+        if (wifimanager.isWifiEnabled()) {
+            wifimanager.setWifiEnabled(false);
+        }
+    }
+
+    /**
+     * wifi 功能是否打开
+     *
+     * @param context
+     * @return
+     */
+    public static boolean isOpenWifi(Context context) {
+        WifiManager wifimanager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        if (wifimanager == null) return false;
+        return wifimanager.isWifiEnabled();
+    }
+
+    /**
+     * 将idAddress转化成string类型的Id字符串
+     *
+     * @param idString
+     * @return
+     */
+    public static String getStringId(int idString) {
+        StringBuilder sb = new StringBuilder();
+        int b = (idString) & 0xff;
+        sb.append(b).append(".");
+        b = (idString >> 8) & 0xff;
+        sb.append(b).append(".");
+        b = (idString >> 16) & 0xff;
+        sb.append(b).append(".");
+        b = (idString >> 24) & 0xff;
+        sb.append(b);
+        return sb.toString();
+    }
+
+    /**
+     * 设置安全性
+     *
+     * @param capabilities
+     * @return
+     */
+    public static String getCapabilitiesString(String capabilities) {
+        if (capabilities.contains("WEP")) {
+            return "WEP";
+        } else if (capabilities.contains("WPA") || capabilities.contains("WPA2") || capabilities.contains("WPS")) {
+            return "WPA/WPA2";
+        } else {
+            return "OPEN";
+        }
+    }
+
+    /**
+     * wifi 是否可见
+     * @param context
+     * @return
+     */
+    public static boolean getIsWifiEnabled(Context context) {
+        WifiManager wifimanager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        return Objects.requireNonNull(wifimanager).isWifiEnabled();
+    }
+
+
+    /**
+     * 去除同名WIFI
+     *
+     * @param oldSr 需要去除同名的列表
+     * @return 返回不包含同命的列表
+     */
+    public static List<ScanResult> noSameName(List<ScanResult> oldSr) {
+        List<ScanResult> newSr = new ArrayList<ScanResult>();
+        for (ScanResult result : oldSr) {
+            if (!TextUtils.isEmpty(result.SSID) && !containName(newSr, result.SSID))
+                newSr.add(result);
+        }
+        return newSr;
+    }
+
+    /**
+     * 判断一个扫描结果中，是否包含了某个名称的WIFI
+     *
+     * @param sr   扫描结果
+     * @param name 要查询的名称
+     * @return 返回true表示包含了该名称的WIFI，返回false表示不包含
+     */
+    public static boolean containName(List<ScanResult> sr, String name) {
+        for (ScanResult result : sr) {
+            if (!TextUtils.isEmpty(result.SSID) && result.SSID.equals(name))
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     * 返回level 等级
+     * 这里暂定为5  意思将 wifi 的信号强度分为五个等级 从小到大依次增强
+     *
+     * @param level
+     */
+    public static int getLevel(int level) {
+        return WifiManager.calculateSignalLevel(level, 5);
+    }
+
+}
